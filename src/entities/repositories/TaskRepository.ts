4c354ee57e892bc2c23c668efd25bbc360ce70fa
Eric Kreis <erickreis793@gmail.com>
2022-02-17 01:13:42 -0300

    tdd: TaskRepository.create implemented

--- src/entities/repositories/TaskRepository.ts
+++ src/entities/repositories/TaskRepository.ts
@@ -1,8 +1,13 @@
 import { ITaskModel } from '../../domains/data/model';
+import { ITaskSchema } from '../../domains/data/schemas/task';
 import { ITaskRepository } from '../../domains/entity/respository';
 
 class TaskRepository implements ITaskRepository {
   constructor(private model: ITaskModel) {}
+
+  public async create(task: ITaskSchema) {
+    return this.model.create(task);
+  }
 }
 
 export default TaskRepository;

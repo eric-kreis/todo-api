d19cba4170360dd6ebf7b9760e04aef1563e4448
Eric Kreis <erickreis793@gmail.com>
2022-02-17 01:21:16 -0300

    tdd: TaskRepository.findById implemented

--- src/entities/repositories/TaskRepository.ts
+++ src/entities/repositories/TaskRepository.ts
@@ -1,9 +1,15 @@
+import DataErrorStruct from '../../data/structs/DataErrorStruct';
 import { ITaskModel } from '../../domains/data/model';
 import { ITaskSchema } from '../../domains/data/schemas/task';
 import { ITaskRepository } from '../../domains/entity/respository';
+import Codes from '../../utils/Codes';
 
 class TaskRepository implements ITaskRepository {
-  constructor(private model: ITaskModel) {}
+  private readonly entity: 'task';
+
+  constructor(private model: ITaskModel) {
+    this.entity = 'task';
+  }
 
   public async create(task: ITaskSchema) {
     return this.model.create(task);
@@ -11,6 +17,12 @@ class TaskRepository implements ITaskRepository {
 
   public async find() {
     return this.model.find();
+  }
+
+  public async findById(id: string) {
+    const task = await this.model.findById(id);
+    if (!task) throw new DataErrorStruct(Codes.NOT_FOUND, `${this.entity} not found`);
+    return task;
   }
 }
 
